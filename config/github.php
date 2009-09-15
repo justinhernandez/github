2ee0f077c7b735b5911c4ebe5c9afdddd43077de
Justin Hernandez <justin@transphorm.com>
2009-09-15 11:47:40 -1000

    upgrading github module to work with Kohana V3

--- config/github.php
+++ config/github.php
@@ -1,29 +1,33 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-/**
- * Default api user for login purposes
- *
- * DEFAULT: NULL
- */
-$config['login'] = 'justinhernandez';
+return array(
 
-/**
- * API token key for github. Found under the account page
- *
- * DEFAULT: NULL
- */
-$config['token'] = '114d9cb09dd6f22e8e7a70560eaafe0b';
+	/**
+	 * Default api user for login purposes
+	 *
+	 * DEFAULT: NULL
+	 */
+	'login' => '',
 
-/**
- * Default format to return the data. Either xml or json.
- *
- * DEFAULT: json
- */
-$config['format'] = 'json';
+	/**
+	 * API token key for github. Found under the account page
+	 *
+	 * DEFAULT: NULL
+	 */
+	'token' => '',
 
-/**
- * Display extra debug info? IN_PRODUCTION also has to be FALSE.
- *
- * DEFAULT: TRUE
- */
-$config['debug'] = TRUE;
+	/**
+	 * Default format to return the data. Either xml or json.
+	 *
+	 * DEFAULT: json
+	 */
+	'format' => 'json',
+
+	/**
+	 * Display extra debug info? IN_PRODUCTION also has to be FALSE.
+	 *
+	 * DEFAULT: TRUE
+	 */
+	'debug' => TRUE,
+
+);
